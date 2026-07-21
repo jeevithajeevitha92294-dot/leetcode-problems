a128f4c4d8e59b9293b33b37b4831dacb30c66cf
jeevithajeevitha92294-dot <jeevithajeevitha92294@gmail.com>
2026-07-21 07:22:46 +0530

    Create 1226-the dining philosopher.java

---
+++ 1226-the dining philosopher.java
@@ -0,0 +1,42 @@
+class DiningPhilosophers {
+
+    private Semaphore[] forks;
+    private Semaphore room;
+
+    public DiningPhilosophers() {
+        forks = new Semaphore[5];
+        for (int i = 0; i < 5; i++) {
+            forks[i] = new Semaphore(1);
+        }
+        room = new Semaphore(4);
+    }
+
+    public void wantsToEat(
+            int philosopher,
+            Runnable pickLeftFork,
+            Runnable pickRightFork,
+            Runnable eat,
+            Runnable putLeftFork,
+            Runnable putRightFork) throws InterruptedException {
+
+        int left = philosopher;
+        int right = (philosopher + 1) % 5;
+
+        room.acquire();
+
+        forks[left].acquire();
+        forks[right].acquire();
+
+        pickLeftFork.run();
+        pickRightFork.run();
+        eat.run();
+        putRightFork.run();
+        putLeftFork.run();
+
+        forks[right].release();
+        forks[left].release();
+
+        room.release();
+    }
+}
+   
